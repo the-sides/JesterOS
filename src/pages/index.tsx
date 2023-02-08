b2c6127bdfe7e5fca782a6332c687d9736c546a3
Jacob Sides <sides798@gmail.com>
2023-02-07 21:16:41 -0500

    HERE remove trpc from index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,8 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 
-import { api } from "../utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   return (
     <>
