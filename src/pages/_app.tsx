f175cf1c7bc839960fc3ea1b4306cf6557115128
Jacob Sides <sides798@gmail.com>
2023-02-07 21:13:00 -0500

    HERE remove next-auth implementations

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,15 @@
 import { type AppType } from "next/app";
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
 
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
+const MyApp: AppType = ({
   Component,
-  pageProps: { session, ...pageProps },
+  pageProps: { ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <Component {...pageProps} />
   );
 };
 
